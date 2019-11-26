9c34f3a8010641da7c12c884cbae3ee0890c78f8
SurOK <nikolay.gers@gmail.com>
2019-11-26 21:53:48 +0000

    разбиение программы на функции

--- cybermans_PSU.py
+++ cybermans_PSU.py
@@ -1,150 +1,152 @@
 import numpy as np
-# import argparse
-import imutils
+import argparse
 import cv2
-import math
 from collections import Counter
+from time import time
 
 
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-q", "--query", required=True,
-#                 help="Path to the query image")
-# args = vars(ap.parse_args())
 def len_of_vect_2d(a, b):
-    return math.sqrt(((a[0] - b[0]) ** 2) + ((a[1] - b[1]) ** 2))
+    """
+    Вычисление длины двумерного вектора
+    :param a: начальная точка (x1, y1)
+    :param b: вторая точка (x2, y2)
+    :return: длина двумерного вектора
+    """
+    return np.sqrt(((a[0] - b[0]) ** 2) + ((a[1] - b[1]) ** 2))
 
 
-def print_numpy_arr(arr):
-    for row in arr:
-        print(' '.join(str(col) for col in row))
+def load_image(file_name):
+    """
+    Загрузка искомого файла
+    :param file_name: полное имя файла
+    :return: файл в формате np
+    """
+    return cv2.imread(file_name)
 
 
-def main():
-    # работа с файлом
-    fn = 'p4.jpg'
-    img0 = cv2.imread(fn)
-    img = img0.copy()
-
-    # формируем начальный и конечный цвет фильтра
-    h_min = np.array((38, 0, 0), np.uint8)
-    h_max = np.array((255, 255, 255), np.uint8)
-
-    # ищем маску
-    img_filter = cv2.bilateralFilter(img, 10, 17, 17)
+def del_background(img, hsv_min, hsv_max):
+    """
+    Удаление фона (за счет инвертирования маски)
+    :param img: np изображение
+    :param hsv_min: минимальное значение цвета фона
+    :param hsv_max: максимальное значение цвета фона
+    :return: np изображение без фона, маска
+    """
+    img_filter = cv2.bilateralFilter(img, 10, 17, 17)  # сглаживание
     hsv = cv2.cvtColor(img_filter, cv2.COLOR_BGR2HSV)
-    thresh = cv2.inRange(hsv, h_min, h_max)
+    thresh = cv2.inRange(hsv, hsv_min, hsv_max)
     mask0 = 255 - thresh  # инвертируем маску
     kernel = np.ones((5, 5), np.uint8)
     mask = cv2.erode(mask0, kernel, iterations=1)
-    res = cv2.bitwise_and(img, img, mask=mask)
+    return cv2.bitwise_and(img, img, mask=mask), mask
 
-    # ищем контуры
-    cnts, h = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+def find_big_rect_cnt(mask, size=0.4):
+    """
+    Ищем большие похожие на прямоугольники контуры
+    :param mask: маска
+    :param size: минимальный процент площади контура от площади маски
+    :return: похожие контуры
+    """
+    cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # ищем контуры
 
     cnts_norm = []
-    # ищем контуры похожие на прямоугольники
+    size_min = len(mask[0]) * len(mask[1]) * size
     for cnt in cnts:
         rect = cv2.minAreaRect(cnt)  # пытаемся вписать прямоугольник
-        # box = cv2.boxPoints(rect)  # поиск четырех вершин прямоугольника
-        # box = np.int0(box)  # округление координат
         area = int(rect[1][0] * rect[1][1])  # вычисление площади
-        if area > len(img[0]) * len(img[1]) * 0.4:
+        if area > size_min:
             cnts_norm.append(cnt)
-            # cv2.drawContours(img, [cnt], -1, (255, 0, 0), 3)
-            # cnt2 = cv2.convexHull(cnt)
-            # cv2.drawContours(img, [cnt2], -1, (0, 255, 0), 3)
-            # cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+    return cnts_norm
 
+
+def create_rect(mask, cnts_norm):
+    """
+    Сделать из контур(а/ов) один хороший четырехугольник
+    :param cnts: контуры
+    :return: контур
+    """
     cnt = None
-    # объединяем большие прямоугольники в один
-    if len(cnts_norm) > 1:
-        # hull_list = []
-        # for i in range(len(cnts_norm)):
-        #     hull = cv2.convexHull(cnts_norm[i])
-        #     hull_list.append(hull)
+    if len(cnts_norm) > 1:  # объединяем большие прямоугольники в один
         approx = np.vstack((cnts_norm))
-        # print(approx)
         cnt = cv2.convexHull(approx)
     else:
         cnt = cnts_norm[0]
 
     cnt = cv2.convexHull(cnt)  # округляем до внешнего контура
-    # пытаемся сделать из многоугольника четырехугольник
     epsilon = 0.05 * cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, epsilon, True)
+    cnt_res = cv2.approxPolyDP(cnt, epsilon, True)  # апроксимилируем
 
-    cnt_res = approx
-    # если больше 4 углов, то удаляем самые близкие к центру вершины
-    if len(approx) > 4:
-        center = [len(img[0]) / 2, len(img[1]) / 2]
-        # print('center', center)
+    if len(cnt_res) > 4:  # если больше 4 углов, то удаляем самые близкие к центру вершины
+        center = [len(mask[0]) / 2, len(mask[1]) / 2]
         approx_norm = []
-        for p in approx:
+        for p in cnt_res:
             approx_norm.append([p, len_of_vect_2d(center, p[0])])
         approx_norm = np.array(approx_norm)
-        # print('approx_norm', approx_norm)
-        # print('approx_norm[]', approx_norm[:, 1])
         while len(approx_norm) > 4:
             np_min = np.argmin(approx_norm[:, 1])
-            # print('min_vect', np_min)
             approx_norm = np.delete(approx_norm, np_min, 0)
-        cnt_res = []
+        cnt_res = []  # переводим в нужный формат
         for p in approx_norm[:, 0]:
             cnt_res.append(p)
         cnt_res = np.int0(cnt_res)
-        # print('approx_norm', approx_norm)
-        # print('cnt_res', cnt_res)
+    return cnt_res
 
-    # рисуем контур
-    # cv2.drawContours(res, [cnt_res], -1, (255, 0, 255), 3)
 
-    pts = cnt_res.reshape(4, 2)
+def create_perspective(img, cnt_res, ratio=1):
+    """
+    Делаем перспективу
+    :param img: исходное изображение
+    :param cnt_res: перспективный контур (четырехугольник)
+    :param ratio: исходное соотношение
+    :return: персмективное np изображение
+    """
+    pts = cnt_res.reshape(4, 2)  # переводим контур в нужный формат
     rect = np.zeros((4, 2), dtype="float32")
 
-    # the top-left point has the smallest sum whereas the
-    # bottom-right has the largest sum
     s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
+    rect[0] = pts[np.argmin(s)]  # верхняя левая точка имеет наименьшую сумму, тогда как
+    rect[2] = pts[np.argmax(s)]  # внизу справа самая большая сумма
 
-    # compute the difference between the points -- the top-right
-    # will have the minumum difference and the bottom-left will
-    # have the maximum difference
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
+    diff = np.diff(pts, axis=1)  # вычислите разницу между точками
+    rect[1] = pts[np.argmin(diff)]  # справа вверху будет иметь минимальную разницу
+    rect[3] = pts[np.argmax(diff)]  # внизу слева будет имеют максимальную разницу
 
-    ratio = 1
-    # multiply the rectangle by the original ratio
-    rect *= ratio
+    rect *= ratio  # умножаем прямоугольник на исходное соотношение
 
+    # теперь, когда у нас есть наш прямоугольник точек, вычислим
+    # ширину нашего нового изображения
     (tl, tr, br, bl) = rect
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
 
-    # ...and now for the height of our new image
+    # ...и высоту
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
 
-    # take the maximum of the width and height values to reach
-    # our final dimensions
+    # возьмите максимальное значение ширины и высоты для достижения наших конечных размеров
     maxWidth = max(int(widthA), int(widthB))
     maxHeight = max(int(heightA), int(heightB))
 
-    # construct our destination points which will be used to
-    # map the screen to a top-down, "birds eye" view
+    # новые точки с высоты птичьего полета
     dst = np.array([
         [0, 0],
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype="float32")
 
-    # calculate the perspective transform matrix and warp
-    # the perspective to grab the screen
+    # вычислите матрицу преобразования перспективы и деформируйте перспективу, чтобы захватить экран
     M = cv2.getPerspectiveTransform(rect, dst)
-    warp = cv2.warpPerspective(res, M, (maxWidth, maxHeight))
+    return cv2.warpPerspective(img, M, (maxWidth, maxHeight))
 
-    warp_filter = cv2.bilateralFilter(warp, 10, 17, 17)
+
+def create_mask_most_color(img):
+    """
+    Создание маски по самому распространенному цвету
+    :param img: np изображение
+    :return:
+    """
+    warp_filter = cv2.bilateralFilter(img, 10, 17, 17)
     warp_hsv = cv2.cvtColor(warp_filter, cv2.COLOR_BGR2HSV)
 
     s0, s1 = warp_hsv[0], warp_hsv[1]  # перве две строчки
@@ -153,56 +155,93 @@ def main():
     sh, ss, sv = np.hstack((s0h, s1h)), np.hstack((s0s, s1s)), np.hstack((s0v, s1v))
     ch, cs, cv = Counter(sh.flat), Counter(ss.flat), Counter(sv.flat)
 
-    print(ch, cs, cv)
+    # print(ch, cs, cv)
 
     bh, bs, bv = None, None, None
-    for i in range(len(ch)-1):
+    for i in range(len(ch) - 1):
         bh = (ch.most_common(i + 1))[-1][0]
         if bh != 0:
             break
-    for i in range(len(cs)-1):
+    for i in range(len(cs) - 1):
         bs = (cs.most_common(i + 1))[-1][0]
         if bs != 0:
             break
-    for i in range(len(cv)-1):
+    for i in range(len(cv) - 1):
         bv = (cv.most_common(i + 1))[-1][0]
         if bv != 0:
             break
 
-    print(bh)
-    print(bs)
-    print(bv)
+    # print(bh)
+    # print(bs)
+    # print(bv)
 
-    mask2 = cv2.inRange(warp_hsv, np.array((bh - 10, bs - 10, bv - 10), np.uint8),
-                        np.array((bh + 10, bs + 10, bv + 10), np.uint8))
-    # res2 = cv2.bitwise_and(warp, warp, mask=mask2)
+    return cv2.inRange(warp_hsv, np.array((bh - 10, bs - 10, bv - 10), np.uint8),
+                       np.array((bh + 10, bs + 10, bv + 10), np.uint8))
 
-    # resized0 = cv2.resize(mask2, (102, 102), interpolation=cv2.INTER_LINEAR_EXACT)
 
-    # resized1 = cv2.resize(mask2, (34, 34), interpolation=cv2.INTER_LINEAR)
-    # kernel = np.ones((1, 1), np.uint8)
-    # kernel2 = np.ones((1, 1), np.uint8)
-    # kernel3 = np.ones((1, 1), np.uint8)
-    # resized2 = cv2.erode(resized1, kernel, iterations=1)
-    # resized3 = cv2.dilate(resized2, kernel2, iterations=1)
-    # resized4 = cv2.erode(resized3, kernel3, iterations=1)
+def good_resize17(mask):
+    """
+    Сжатие маски к 17 на 17
+    :param mask: маска
+    :return:
+    """
+    resized = cv2.resize(mask, (17, 17), interpolation=cv2.INTER_BITS)
+    return np.where(resized < 150, 0, resized)
 
-    resized = cv2.resize(mask2, (17, 17), interpolation=cv2.INTER_BITS)
-    # resized2 = cv2.resize(resized0, (17, 17), interpolation=cv2.INTER_BITS)
 
-    result = np.where(resized > 150, 1, 0)
-    print(result)
-    # print(resized)
-    # выводим изображения
-    # cv2.imshow('img', img)
-    cv2.imshow('hsv_filter', warp_hsv)
-    cv2.imshow('res', res)
-    cv2.imshow('mask2', mask2)
-    # cv2.imshow('res2', res2)
-    # cv2.imshow('resized0', resized0)
-    cv2.imshow('resized', resized)
-    # cv2.imshow('resized2', resized2)
-    # cv2.imshow('result2', result2)
+def find_bad_block_and_to_bit(mask):
+    """
+    Выявление плохих блоков и превращение маски в 0 и 1
+    :param mask: np маска
+    :return:
+    """
+    bad = []
+    for i in range(len(mask) - 1):
+        for j in range(len(mask) - 1):
+            if mask[i, j] == mask[i + 1, j] == mask[i, j + 1] == mask[i + 1, j + 1]:
+                bad.append([i, j])
+    # скорее всего плохие блоки будут на одной линии на оси ординат
+    # print(bad)
+    # if len(bad) > 0:
+    #     j = bad[0][1]
+    #     while j < len(mask) - 1:
+    #         for i in range(len(mask)):
+    #             mask[i, j] = mask[i, j + 1]
+    #         j += 1
+    #     for i in range(len(mask)):
+    #         mask[i, 16] = 0
+
+    return np.where(mask > 150, 1, 0)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='labirint')
+
+    # parser.add_argument('-i', action="store", dest="count", default=2, type=int, help='sum')
+    parser.add_argument('img', type=str, help="Path to the query image")
+    args = parser.parse_args()
+    # print(args)
+
+    time_before = time()
+
+    img0 = load_image(args.img)
+    h_min = np.array((38, 0, 0), np.uint8)
+    h_max = np.array((255, 255, 255), np.uint8)
+    img1, mask1 = del_background(img0, h_min, h_max)
+    cnts1 = find_big_rect_cnt(mask1)
+    cnt1 = create_rect(mask1, cnts1)
+    img2 = create_perspective(img1, cnt1)
+    mask2 = create_mask_most_color(img2)
+    mask3 = good_resize17(mask2)
+    # print(mask3)
+    print(find_bad_block_and_to_bit(mask3))
+
+    time_after = time()
+    print(time_after - time_before, ' seconds')
+
+    print('Нажмите любую клавишу, чтобы выйти')
+    cv2.imshow('mask', mask2)
+    cv2.imshow('result', mask3)
     cv2.waitKey()  # ждем нажатие любой клавиши
     cv2.destroyAllWindows()
 
